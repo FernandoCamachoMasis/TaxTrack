ca908ff7a02128274979bcbba58e506b644075ba
FernandoCamachoMasis <fercama2012@gmail.com>
2023-09-25 13:56:51 -0600

    actualizado

--- app/src/main/java/com/example/taxtrack/passwordChange.kt
+++ app/src/main/java/com/example/taxtrack/passwordChange.kt
@@ -1,11 +1,22 @@
 package com.example.taxtrack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class passwordChange : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_password_change)
+
+        val buttonBack = findViewById<Button>(R.id.buttonBack) // Cambia el tipo a Button
+
+        buttonBack.setOnClickListener {
+            // Cuando se hace clic en el botón de retroceso, inicia LoginActivity
+            val intent = Intent(this, login::class.java)
+            startActivity(intent)
+            finish() // Esto finalizará la actividad actual (ChangePasswordActivity)
+        }
     }
 }
